9e76990015f3cd76fb1e2f4c026ea3b732d95103
josegcpa <jose.almeida@research.fchampalimaud.pt>
2025-01-10 15:42:07 +0000

    making classification generic

--- eucaim_eval/classification.py
+++ eucaim_eval/classification.py
@@ -26,6 +26,10 @@ class ClassificationMetrics(AbstractMetrics):
             "average_precision": average_precision_score,
         }
 
+    @property
+    def proba_metrics(self):
+        return ["auroc", "average_precision"]
+
     def calculate_metrics(
         self,
         pred: list[float] | np.ndarray,
@@ -33,13 +37,59 @@ class ClassificationMetrics(AbstractMetrics):
         metrics: list[str] | None = None,
         ci: float = 0.95,
     ) -> dict:
+        """
+        Calculate classification metrics.
+
+        Args:
+            pred (list[float] | np.ndarray): prediction probabilities.
+            gt (list[float  |  int] | np.ndarray): target variables.
+            metrics (list[str] | None, optional): list of metric strings.
+                Defaults to None.
+            ci (float, optional): confidence intervals. Defaults to 0.95.
+
+        Raises:
+            ValueError: if pred and gt have different lengths.
+
+        Returns:
+            dict: dictionary with entries for "metrics", which are calculated
+                from the raw data, and "metrics_mean", "metrics_median",
+                "metrics_sd", and "metrics_ci", which are calculated using
+                bootstraping.
+        """
         if metrics is None:
             metrics = self.metric_match.keys()
-        output_dict = {}
+        q = (1 - ci) / 2
+        q = q, 1 - q
+        output_dict = {
+            "metrics": {},
+            "metrics_mean": {},
+            "metrics_median": {},
+            "metrics_sd": {},
+            "metrics_ci": {},
+        }
+        if self.n_classes == 2:
+            pred, proba = pred > 0.5, pred
+        else:
+            proba = pred
+            pred = np.argmax(pred, axis=1)
+            if len(gt.shape) > pred.shape:
+                gt = np.argmax(gt, axis=1)
         for metric in metrics:
             if metric in self.metric_match:
-                output_dict[metric] = self.metric_match[metric](
-                    gt, pred, **self.params[metric]
+                p = proba if metric in self.proba_metrics else pred
+                output_dict["metrics"][metric] = self.metric_match[metric](
+                    gt, p, **self.params[metric]
+                )
+                bootstrap_sample = self.bootstrap(
+                    self.metric_match[metric], 1000, 0.5, gt, p
+                )
+                output_dict["metrics_mean"][metric] = np.mean(bootstrap_sample)
+                output_dict["metrics_median"][metric] = np.median(
+                    bootstrap_sample
+                )
+                output_dict["metrics_sd"][metric] = np.std(bootstrap_sample)
+                output_dict["metrics_ci"][metric] = np.quantile(
+                    bootstrap_sample, q
                 )
             else:
                 raise ValueError(f"Unknown metric: {metric}")
