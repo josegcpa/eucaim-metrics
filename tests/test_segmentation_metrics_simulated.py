dabd6f9d0618566da5700d5a70e4ff5b219185ee
josegcpa <jose.almeida@research.fchampalimaud.pt>
2025-01-13 17:01:10 +0000

    better testing for segmentation models

--- tests/test_segmentation_metrics_simulated.py
+++ tests/test_segmentation_metrics_simulated.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pprint import pprint
+from pytest import approx
 from eucaim_eval.segmentation import SegmentationMetrics
 from skimage.draw import random_shapes
 from scipy.ndimage import binary_erosion
@@ -36,80 +36,120 @@ def generate_data(n_classes: int = 2):
     return output, output_pred
 
 
-def get_example_data():
-    output = np.array(
-        [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ]
-    )
-    output_pred = np.array(
-        [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        ]
-    )
-    return output, output_pred
+binary_output = np.array(
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+)
+binary_output_pred = np.array(
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+)
+
+mc_output = np.array(
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 3, 3, 3, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+    ]
+)
+mc_output_pred = np.array(
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0],
+        [0, 0, 0, 0, 1, 1, 0, 0, 3, 3, 3, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    ]
+)
+
+real_values = {
+    "multiclass": {
+        "match_region": {
+            "dice": {
+                0: 0.9838709677419355,
+                1: 0.8660714285714286,
+                2: 0.8,
+                3: 1.0,
+            },
+            "hd": {
+                0: 1.0,
+                1: 1.0,
+                2: 1.0,
+                3: 0.0,
+            },
+            "normalised_surface_distance": {
+                0: 0.9682539682539683,
+                1: 0.8571428571428571,
+                2: 0.8,
+                3: 1.0,
+            },
+        },
+        "standard": {
+            "dice": {
+                0: 0.9838709677419355,
+                1: 0.8695652173913043,
+                2: 0.8,
+                3: 1.0,
+            },
+            "hd": {
+                0: 1.0,
+                1: 1.0,
+                2: 1.0,
+                3: 0.0,
+            },
+            "normalised_surface_distance": {
+                0: 0.9682539682539683,
+                1: 0.8571428571428571,
+                2: 0.8,
+                3: 1.0,
+            },
+        },
+    }
+}
 
 
-def get_example_mc_data():
-    output = np.array(
-        [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0],
-            [0, 0, 0, 0, 1, 1, 0, 0, 3, 3, 3, 0],
-            [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-        ]
-    )
-    output_pred = np.array(
-        [
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 0],
-            [0, 0, 0, 0, 1, 1, 0, 0, 3, 3, 3, 0],
-            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-        ]
-    )
-    return output, output_pred
-
-
-def test_segmentation_binary_metrics():
-    output, output_pred = get_example_data()
+def test_segmentation_binary_metrics_standard():
+    output, output_pred = binary_output, binary_output_pred
     classification_metrics = SegmentationMetrics(
         n_workers=4,
         params={"normalised_surface_distance": {"max_distance": 0.5}},
@@ -120,8 +160,8 @@ def test_segmentation_binary_metrics():
     metrics = classification_metrics.calculate_metrics([output_pred], [output])
 
 
-def test_segmentation_mc_metrics():
-    output, output_pred = get_example_mc_data()
+def test_segmentation_mc_metrics_standard():
+    output, output_pred = mc_output, mc_output_pred
     classification_metrics = SegmentationMetrics(
         n_workers=4,
         params={"normalised_surface_distance": {"max_distance": 0.5}},
@@ -130,10 +170,18 @@ def test_segmentation_mc_metrics():
     )
 
     metrics = classification_metrics.calculate_metrics([output_pred], [output])
+
+    gt = real_values["multiclass"]["standard"]
+    for k in gt["dice"].keys():
+        assert metrics["dice"]["mean"][k] == approx(gt["dice"][k])
+        assert metrics["hausdorff_distance"]["mean"][k] == approx(gt["hd"][k])
+        assert metrics["normalised_surface_distance"]["mean"][k] == approx(
+            gt["normalised_surface_distance"][k]
+        )
 
 
 def test_segmentation_binary_metrics_match_region():
-    output, output_pred = get_example_data()
+    output, output_pred = binary_output, binary_output_pred
     classification_metrics = SegmentationMetrics(
         n_workers=4,
         params={"normalised_surface_distance": {"max_distance": 0.5}},
@@ -147,7 +195,7 @@ def test_segmentation_binary_metrics_match_region():
 
 
 def test_segmentation_mc_metrics_match_region():
-    output, output_pred = get_example_mc_data()
+    output, output_pred = mc_output, mc_output_pred
     classification_metrics = SegmentationMetrics(
         n_workers=4,
         params={"normalised_surface_distance": {"max_distance": 0.5}},
@@ -158,3 +206,11 @@ def test_segmentation_mc_metrics_match_region():
     metrics = classification_metrics.calculate_metrics(
         [output_pred], [output], match_regions=True
     )
+
+    gt = real_values["multiclass"]["match_region"]
+    for k in gt["dice"].keys():
+        assert metrics["dice"]["mean"][k] == approx(gt["dice"][k])
+        assert metrics["hausdorff_distance"]["mean"][k] == approx(gt["hd"][k])
+        assert metrics["normalised_surface_distance"]["mean"][k] == approx(
+            gt["normalised_surface_distance"][k]
+        )
